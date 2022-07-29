4aecd1f90ccbcf67eca56400d3eef59e4d813893
Incorrect111 <alexeev.andrey1987@gmail.com>
2022-07-29 23:27:25 +0300

    create items arr & add card to home page

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,3 +1,32 @@
 <template>
   <h1>hello home</h1>
+  <div class="card__wrapper" v-for="item in items" :key="item.id" >
+  <Card />
+  </div>
 </template>
+
+<script>
+import Card from '@/components/UI/Card.vue'
+export default {
+  components: { Card },
+
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          title: 'Archer',
+          descr: 'Description for Archer',
+          level: 4
+        },
+        {
+          id: 2,
+          title: 'Wizard',
+          descr: 'Description for Wizard',
+          level: 5
+        }
+      ]
+    }
+  }
+}
+</script>
