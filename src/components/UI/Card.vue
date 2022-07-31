ae791c92ff79ad8cd06dadcd1d3c68d34f6155b7
Incorrect111 <alexeev.andrey1987@gmail.com>
2022-07-31 17:43:48 +0300

    add img

--- src/components/UI/Card.vue
+++ src/components/UI/Card.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="card">
+    <div v-if="item.img" class="card-img-wrapper">
+      <img class="card-img" :src="item.img" :alt="item.title" />
+    </div>
     <span class="card-name">{{ item.level }} lvl</span>
     <span class="card-title">{{ item.title }}</span>
     <div class="card-body">
@@ -11,11 +14,10 @@
 <script>
 export default {
   props: {
-  item: {
-    type: Object,
-    required: true
-  }
-
+    item: {
+      type: Object,
+      required: true
+    }
   }
 }
 </script>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -8,26 +8,14 @@
 </template>
 
 <script>
+import items from '@/seeders/items.json'
 import Card from '@/components/UI/Card.vue'
 export default {
   components: { Card },
 
   data() {
     return {
-      items: [
-        {
-          id: 1,
-          title: 'Archer',
-          descr: 'Description for Archer',
-          level: 4
-        },
-        {
-          id: 2,
-          title: 'Wizard',
-          descr: 'Description for Wizard',
-          level: 5
-        }
-      ]
+     items: items
     }
   }
 }
