5f8bb44ebfdf897b2cd29fc08a6c9fd12d35254b
Incorrect111 <alexeev.andrey1987@gmail.com>
2022-08-04 11:08:40 +0300

    Add push to 404 page

---
+++ src/components/UI/CardStats.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card-stats">
+    <div v-for="(stat, index) in item.info" :key="index" class="one-third">
+      <div class="stat-value">{{ stat.value }}</div>
+      <div class="stat">{{ stat.title }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
