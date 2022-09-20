93524f61b5b31172a81206d74376898728ed3b94
Incorrect111 <alexeev.andrey1987@gmail.com>
2022-09-20 15:28:42 +0300

    change about page

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
 	transpileDependencies: true,
-	publicPath: '/to-code-vue3-clash-of-clans/',
+	publicPath: '/',
 	productionSourceMap: false,
 	assetsDir: 'assets',
 	filenameHashing: false
